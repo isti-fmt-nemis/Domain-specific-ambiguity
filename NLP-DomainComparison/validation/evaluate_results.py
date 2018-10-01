82948bd5c1915a956cce89d01ad4ff037e34d082
alessioferrari <alessiofer@gmail.com>
2018-10-01 10:26:54 +0200

    added stub to automatically evaluate results

---
+++ NLP-DomainComparison/validation/evaluate_results.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 21, 2018
+
+@author: alessioferrari
+
+This module includes all the methods to evaluate the results of the AMT task with
+respect to the ranked lists of terms. 
+'''
+
+
+def evaluate_results_top_bottom():
+    return 0
+
+def compute_AMT_scores():
+    return 0
